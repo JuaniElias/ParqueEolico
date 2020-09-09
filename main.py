b9a8b14bad35803a954d73fbbe8f41861a74588d
Juan Ignacio Elias <Juanignacioelias@hotmail.com>
2020-09-08 22:35:22 -0300

    Minor changes & code cleanup

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ rugosidad = 0.0024
 chances_crossover = 2
 chances_mutacion = 0.20
 filas = 10
-col = 10
-cte_long_estela = 12            #podemos jugar con este valor
+columnas = 10
+cte_long_estela = 12            # Podemos jugar con este valor
 cant_molinos = 25
 viento_puro = 20
 viento = [0] * 10
 
 
-array_fitness = [0] * tam_poblacion             #se guardan los totales de energia de todos los cromosomas
-array_poblacion = [0] * tam_poblacion           #se guardan todos los cromosomas
-array_energia_molino = [0] * tam_poblacion      #se guarda la energia producida por cada molino
+array_fitness = [0] * tam_poblacion             # Se guardan los totales de energia de todos los cromosomas
+array_poblacion = [0] * tam_poblacion           # Se guardan todos los cromosomas
+array_energia_molino = [0] * tam_poblacion      # Se guarda la energia producida por cada molino
 
 
 class Molino:
@@ -77,7 +77,6 @@ def retorna_energia(velocidad):
     for i in range(0, 10):
         if velocidad == molinos.velocidad_potencia[i].velocidad:
             return molinos.velocidad_potencia[i].potencia
-            break
     if 13 <= velocidad <= 25:
         return molinos.velocidad_potencia[9].potencia
     elif velocidad > 25 or 0 <= velocidad < 4:
@@ -90,30 +89,33 @@ def calcula_velocidad_viento(velocidad_inicial, x):
     return velocidad_final
 
 
-def fitness(k):                     #pensar para 3 molinos consecutivos
+def fitness(k):                     # Pensar para 3 molinos consecutivos
     viento_actual = viento_puro
     energia = 0
-    m_energia = np.zeros((10, 10))
+    m_energia = np.zeros((filas, columnas))
     m = array_poblacion[k]
-    #def matriz de vientos que me arme un 10x10 con todos los valores del viento
+    # Def matriz de vientos que me arme un 10x10 con todos los valores del viento
     for i in range(0, filas):
         flag = True
         cont = 0
-        for j in range(0, col):
-            if m[i][j] == 1:
-                if flag or cte_long_estela * molinos.diametro < cont * molinos.distancia_minima:    # entra si el viento es puro
+        for j in range(0, columnas):
+
+            if m[i][j] == 1:  # Si hay un molino en la posición
+                # Entra si el viento es puro
+                if flag or cte_long_estela * molinos.diametro < cont * molinos.distancia_minima:
                     flag = False
                     energia = energia + retorna_energia(viento_puro)
                     m_energia[i][j] = retorna_energia(viento_puro)
-                else:                                                                               # entra si el viento es turbulento
-                        velocidad_final = calcula_velocidad_viento(viento_puro, cont)
-                        energia = energia + retorna_energia(velocidad_final)
-                        m_energia[i][j] = retorna_energia(velocidad_final)
-                cont = 1
-
+                # Entra si el viento es turbulento
+                else:
+                    velocidad_final = calcula_velocidad_viento(viento_puro, cont)
+                    energia = energia + retorna_energia(velocidad_final)
+                    m_energia[i][j] = retorna_energia(velocidad_final)
+                cont += 1
             else:
                 if cont != 0:
                     cont += 1
+
     array_energia_molino[k] = m_energia
     array_fitness[k] = energia
 
@@ -137,30 +139,36 @@ def crossover():
 
         if cros < chances_crossover:
 
-                                                # CROSSOVER POR COLUMNAS
-            hijo1_molinos = np.concatenate((array_poblacion[i], array_poblacion[i+1]), axis=1)       #matriz 20x10 molinos
-            hijo1_potencia = np.concatenate((array_energia_molino[i], array_energia_molino[i+1]), axis=1)          #matriz 20x10 potencia
-            sumatoria_h1 = hijo1_potencia.sum(axis=0)        #devuelve un arreglo con la sumatoria de todas las columnas
+            #                               -- CROSSOVER POR COLUMNAS --
+            # Matriz 20x10 molinos
+            hijo1_molinos = np.concatenate((array_poblacion[i], array_poblacion[i+1]), axis=1)
+            # Matriz 20x10 potencia
+            hijo1_potencia = np.concatenate((array_energia_molino[i], array_energia_molino[i+1]), axis=1)
+
+            sumatoria_h1 = hijo1_potencia.sum(axis=0)   # Devuelve un arreglo con la sumatoria de todas las columnas
             permutacion = np.argsort(-sumatoria_h1)
 
-            hijo1 = np.zeros((10, 10))
+            # hijo1 = hijo1[:, permutacion]
+            # hij02 = hijo1[permutacion]
+
+            hijo1 = np.zeros((filas, columnas))
             for columna in range(0, 10):
                 for fila in range(0, 10):
                     hijo1[fila][columna] = hijo1_molinos[fila][permutacion[columna]]
 
+            #                                 -- CROSSOVER POR FILAS --
+            # Matriz 10x20 molinos
+            hijo2_molinos = np.concatenate((array_poblacion[i], array_poblacion[i + 1]), axis=0)
+            # Matriz 10x20 potencia
+            hijo2_potencia = np.concatenate((array_energia_molino[i], array_energia_molino[i + 1]), axis=0)
 
-                                                #CROSSOVER POR FILAS
-            hijo2_molinos = np.concatenate((array_poblacion[i], array_poblacion[i + 1]), axis=0)    # matriz 10x20 molinos
-            hijo2_potencia = np.concatenate((array_energia_molino[i], array_energia_molino[i + 1]), axis=0)  # matriz 10x20 potencia
-            sumatoria_h2 = hijo2_potencia.sum(axis=1)  # devuelve un arreglo con la sumatoria de todas las filas
+            sumatoria_h2 = hijo2_potencia.sum(axis=1)  # Devuelve un arreglo con la sumatoria de todas las filas
             permutacion = np.argsort(-sumatoria_h2)
 
-            hijo2 = np.zeros((10, 10))
+            hijo2 = np.zeros((filas, columnas))
             for fila in range(0, 10):
                 for columna in range(0, 10):
                     hijo2[fila][columna] = hijo2_molinos[permutacion[fila]][columna]
-
-
 
 
 poblacion_inicial()
